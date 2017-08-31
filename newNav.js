ec72cd840a6b1092a38bc408347437fa1cb3c4e5
hafalquran1bulan <30674832+hafalquran1bulan@users.noreply.github.com>
2017-08-31 17:32:12 +0700

    Update newNav.js

--- newNav.js
+++ newNav.js
@@ -1,3 +1,4 @@
+
 (function($,sr){
   var debounce = function (func, threshold, execAsap) {
       var timeout;
@@ -24,19 +25,24 @@
 	$drawerRight = $('#drawer-right');
 
 	function setSlideNav(){
+		var scrollTop = jQuery(window).scrollTop();
+
 		jQuery(".toggleDrawer").click(function(e){
 			//alert($wrapper.css('marginRight'));
 			e.preventDefault();
 
 			if($wrapper.css('marginLeft')=='0px'){
-				$drawerRight.animate({marginRight : 0},500);
-				$wrapper.animate({marginLeft : -300},500);
+				$drawerRight.animate({marginRight : 0},400);
+				$wrapper.animate({marginLeft : -300},400);
+				$('#headerA #main_navbar').removeClass('navbar-fixed-top');
+
 			}
 			else{
-				$drawerRight.animate({marginRight : -300},500);
-				$wrapper.animate({marginLeft : 0},500);
+				$drawerRight.animate({marginRight : -300},400);
+				$wrapper.animate({marginLeft : 0},400);
+			 
 			}
-			
+
 		})
 	} 
 	function setHeaderBackground() {		
@@ -51,6 +57,14 @@
 			jQuery('#Nsearch .srccolor').removeClass('whtr');
 			jQuery('#Nsearch .search-form').addClass('dakr');	
 			jQuery('#Nsearch .form-control').removeClass('wform'); 
+			jQuery('#headerA .no-border-radius').removeClass('twbg');
+
+			if($wrapper.css('marginLeft')=='-300px'){
+				$drawerRight.animate({marginRight : -300},400);
+				$wrapper.animate({marginLeft : 0},400);
+			 
+
+			}
 		} else {
 			jQuery('#headerA .topA').removeClass('solid');
 			jQuery('#headerA .dropdown-menu').addClass('wbg');
@@ -60,6 +74,9 @@
 			jQuery('#Nsearch .srccolor').addClass('whtr');
 			jQuery('#Nsearch .search-form').removeClass('dakr');	
 			jQuery('#Nsearch .form-control').addClass('wform');
+			jQuery('#headerA .no-border-radius').addClass('twbg');
+
+
 		}
 	}
 	setSlideNav();
